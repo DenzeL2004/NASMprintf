2846f3f7528f44908e1908fd7b3a13dc29051f3d
DenzeL2004 <work20022004@mail.ru>
2023-03-29 00:29:34 +0300

    call_Cprint

--- printf.s
+++ printf.s
@@ -3,61 +3,66 @@
 %include "src/generals.s"
 %include "config.s"
 
+global _printf
 
-static _specificator_processing:function
+static print:function
 
-static _print_char:function
+static specificator_processing:function
 
-static _print_string:function
+static print_char:function
 
-static _print_int_dec_num:function
+static print_string:function
 
-static _print_hex_rep:function
+static print_int_dec_num:function
 
-static _print_oct_rep:function
+static print_hex_rep:function
 
-static _print_bin_rep:function
+static print_oct_rep:function
 
-static _print_num_rep:function
+static print_bin_rep:function
+
+static print_num_rep:function
 
 section .text
 
-global _start                  ; predefined entry point name for ld
+;------------------------------------------------------------------------
+;wrapper for C function call. SYSCALL 
+;------------------------------------------------------------------------
+_printf:
 
-_start:    
-
-    push 127d
-    push 33d
-    push 100d
-    push 3802d
-    push string
-    push -1d
+    pop qword [return_address]          ;save return address
     
-    push msg
-    
-    call _print
+    push r9
+    push r8 
+    push rcx
+    push rdx
+    push rsi
+    push rdi
 
-    add sp, 0x08 * 8d
+    call printf wrt ..plt              
 
-    EXIT 0
+    add rsp, 0x08 * 6d                  ;clearing the stack of arguments
 
+    push qword [return_address]         ;save return address
+
+    ret
 
 ;------------------------------------------------------------------------
-;print out the string with parameters in the format string
+;print out the string with parameters in the format string. CDECL
 ;------------------------------------------------------------------------
 ;Entre:     [rbp + 0x10] - format string address, any number of input parameters
 ;Exit:      none
 ;Destroy:   rax, rdx, rsi, rdi
 ;           r9  - current position in format string, r10 - last not specificator symbol
 ;           r12 - pointer to input print's parameters 
-;--------------------------------------------------------------
+;------------------------------------------------------------------------
 
-_print:
+printf:
 
     push rbp
     mov  rbp, rsp  
 
-    mov r12, 0x10                    ;parameters offset
+    mov r12, 0x10                   ;parameters offset
 
     mov r9, qword [rbp + r12]       ;start format string address
     mov r10, r9                     ;start format string address
@@ -82,7 +87,7 @@ _print:
 
     add r12, 0x08                   ;set pointer to input print's parameters
     mov rbx, qword [rbp + r12]
-    call _specificator_processing
+    call specificator_processing
 
     mov r10, r9                     ;last not specificator symbol address
     dec r9                          ;correct string address
@@ -120,7 +125,7 @@ _print:
 ;           r9 - current position in format string, r12 - pointer to input print's parameters 
 ;--------------------------------------------------------------
    
-_specificator_processing:
+specificator_processing:
 
     inc r9                          ;-----------------------         
     mov al, byte [r9]               ;get specificator's mode 
@@ -133,38 +138,38 @@ _specificator_processing:
 
 .print_switch_char:
 
-    call _print_char
+    call print_char
     jmp .end_switch
 
 .print_switch_string:
 
-    call _print_string
+    call print_string
     jmp .end_switch
 
 .print_switch_dec_num:
 
-    call _print_int_dec_num
+    call print_int_dec_num
     jmp .end_switch
 
 .print_switch_hex_num:
 
-    call _print_hex_rep
+    call print_hex_rep
     jmp .end_switch
 
 .print_switch_oct_num:
 
-    call _print_oct_rep
+    call print_oct_rep
     jmp .end_switch
 
 .print_switch_bin_num:
 
-    call _print_bin_rep
+    call print_bin_rep
     jmp .end_switch
 
 .print_switch_default:
     xor rbx, rbx
     mov bl, byte [r9]
-    call _print_char
+    call print_char
 
     sub r12, 0x08                   ;another specifier does not require a parameter
 
@@ -214,7 +219,7 @@ section .text
 ;Destroy:   rax, rbx, rdx, rsi, rdi 
 ;------------------------------------------------------------------------
    
-_print_char:
+print_char:
 
     mov byte [temp_string], bl
 
@@ -234,7 +239,7 @@ _print_char:
 ;Destroy:   rax, rdx, rsi, rdi 
 ;------------------------------------------------------------------------
    
-_print_string:
+print_string:
 
     mov rdi, rbx                    ;-------------------
     call _get_len                   ;get string's length
@@ -256,7 +261,7 @@ _print_string:
 ;Destroy:   rax, rdx, rsi, rdi 
 ;------------------------------------------------------------------------
    
-_print_int_dec_num:
+print_int_dec_num:
 
     test ebx, dword [Int_min_val]   ;------------------------
     je .not_negative                ;chec is negative number?
@@ -268,7 +273,7 @@ _print_int_dec_num:
     jne .not_int_min                 ;check value is integer?
 
     mov rbx, Int_min_str            ;-------------------
-    call _print_string              ;print Int_min_value
+    call print_string              ;print Int_min_value
 
     ret
 
@@ -315,7 +320,7 @@ _print_int_dec_num:
     inc rdi                         ;------------------
     mov rbx, rdi                    ;get correct temp_string address
 
-    call _print_string              ;print temp_string 
+    call print_string              ;print temp_string 
 
     ret 
 
@@ -328,11 +333,11 @@ _print_int_dec_num:
 ;Destroy:   rax, rbx, cx, rdx, rsi, rdi 
 ;------------------------------------------------------------------------
    
-_print_hex_rep:
+print_hex_rep:
 
     mov rdx, Hex_digits
     mov cl, 0x04
-    call _print_num_rep
+    call print_num_rep
 
     mov rdi, CONSOL_DESCRIPTOR
     
@@ -354,11 +359,11 @@ _print_hex_rep:
 ;Destroy:   rax, rbx, cx, rdx, rsi, rdi 
 ;------------------------------------------------------------------------
    
-_print_oct_rep:
+print_oct_rep:
 
     mov rdx, Oct_digits
     mov cl, 0x03
-    call _print_num_rep
+    call print_num_rep
 
     mov rdi, CONSOL_DESCRIPTOR
     
@@ -380,15 +385,15 @@ _print_oct_rep:
 ;Destroy:   rax, rbx, cx, rdx, rsi, rdi 
 ;------------------------------------------------------------------------
    
-_print_bin_rep:
+print_bin_rep:
 
     mov rdx, Bin_digits
     mov cl, 0x01
-    call _print_num_rep
+    call print_num_rep
 
     mov rdi, CONSOL_DESCRIPTOR
 
-    mov byte [temp_string],  'o'
+    mov byte [temp_string],  'b'
     mov rsi, temp_string
 
     mov rdx, 0x01
@@ -406,7 +411,7 @@ _print_bin_rep:
 ;Destroy:   rax, rbx, cx, rdx, rsi, rdi 
 ;------------------------------------------------------------------------
    
-_print_num_rep:
+print_num_rep:
 
     std
 
@@ -438,7 +443,7 @@ _print_num_rep:
     inc rdi                         ;------------------
     mov rbx, rdi                    ;get correct temp_string address
 
-    call _print_string              ;print temp_string
+    call print_string              ;print temp_string
 
     ret 
 
@@ -450,5 +455,6 @@ temp_string:    times BUFFER_SIZE db 0x00
 msg:        db "%a%z%# %d %s  %x %d%%%c%b", 0xa, TERM_CHAR
 string:     db "Love", TERM_CHAR
 
+return_address: dq 0x00
 
 
